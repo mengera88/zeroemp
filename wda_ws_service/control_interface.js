59732b5ab9e76f1ae336fe627dbd2de11165556d
hzgujing <hzgujing@corp.netease.com>
2016-12-29 16:31:52 +0800

    added:ios真机调试架构调整，封装ws

---
+++ wda_ws_service/control_interface.js
@@ -0,0 +1,177 @@
+/**
+ * 控制真机接口服务工具，提供给wda_service服务调用
+ * @author  jessica(hzgujing@corp.netease.com)
+ */
+
+const fetch = require('node-fetch');
+
+class DeviceControl {
+    constructor(url, socket) {
+        this.deviceUrl  = url;
+        this.socket = socket;
+    }
+    //返回Home键
+    returnHome() {
+        fetch(this.deviceUrl + '/homescreen', {
+            method: 'post'
+        })
+        .then((data) => {
+            console.log('request success')
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //单击事件
+    handleClick(x, y) {
+        console.log("单击坐标信息("+ x, y + ")")
+        fetch(this.deviceUrl + '/deviceControl/click', {
+            method: 'post',
+            body: JSON.stringify({
+                x: x,
+                y: y
+            })
+        })
+        .then((data) => {
+            console.log('request success')
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //双击事件
+    handleDoubleClick(x, y) {
+        console.log("双击击坐标信息("+ x, y + ")")
+        fetch(this.deviceUrl + '/deviceControl/doubleClick', {
+            method: 'post',
+            body: JSON.stringify({
+                x: x,
+                y: y
+            })
+        })
+        .then((data) => {
+            console.log('request success')
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //长按
+    handleTouchAndHold(x, y, duration) {
+        duration = parseFloat(duration/1000);
+        fetch(this.deviceUrl + '/DeviceControl/touchAndHold', {
+            method: 'post',
+            body: JSON.stringify({
+                x: x,
+                y: y,
+                duration: duration
+            })
+        })
+        .then((data) => {
+            console.log('request success')
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //拖拽
+    handleDrag(fromX, fromY, toX, toY, pressDuration, dragDuration) {
+        pressDuration = parseFloat(pressDuration/1000);
+        dragDuration = parseFloat(dragDuration/1000);
+        console.log(fromX, fromY, toX, toY, pressDuration, dragDuration)
+        fetch(this.deviceUrl + '/deviceControl/dragFromToForDuration', {
+            method: 'post',
+            body: JSON.stringify({
+                fromX: fromX,
+                fromY: fromY,
+                toX: toX,
+                toY: toY,
+                pressDuration: pressDuration,
+                dragDuration: dragDuration
+            })
+        })
+        .then((data) => {
+            console.log('request success')
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //获取设备旋转状态
+    getOrientatioin() {
+        fetch(this.deviceUrl + '/deviceControl/getOrientation', {
+            method: 'get'
+        })
+        .then((data) => data.text())
+        .then((text) => {
+            let orientation = JSON.parse(text).value;
+            return orientation;
+        })
+        .catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //设置设备旋转状态
+    /**
+     * 传入的参数可选值：
+     * PORTRAIT
+     * LANDSCAPE
+     * UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT
+     * UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN 
+    */
+    setOrientation(orientation) {
+        fetch(this.deviceUrl + '/deviceControl/setOrientation', {
+            method: 'post',
+            body: JSON.stringify({
+                orientation: orientation
+            })
+        })
+        .then((data) => data.text())
+        .then((text) => {
+            console.log(text)
+            let status = JSON.parse(text).status;
+            console.log(status)
+            if(status == 777) {
+                console.log('cantChange')
+                this.socket.emit('cantChange');
+            }
+        })
+        .catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+
+    //输入 
+    /**
+     * value为字符数组 "value":
+     * [
+     * "h","t","t","p",":","/","/","g","i","t","h","u","b",".","c","o","m","\\n"
+     * ] 
+     */
+    handleKeys(value) {
+        console.log(value)
+        fetch(this.deviceUrl + '/deviceControl/keys', {
+            method: 'post',
+            body: JSON.stringify({
+                value: value
+            })
+        })
+        .then((data) => {
+            console.log('request success')
+            console.log(data)
+        }).catch((err) => {
+            console.log('request failed')
+            console.log(err)
+        })
+    }
+}
+
+module.exports = DeviceControl
---
+++ web/js/util/index.js
@@ -0,0 +1,63 @@
+/**
+ * Utilities
+ * @module  utils
+ * @author  jessica(hzgujing@corp.netease.com)
+ */
+
+'use strict';
+
+/**
+ * 移除数组中一项
+ * @param   {Array} arr 数组对象
+ * @param   {*}     o   要移除的数组项目
+ * @returns {*} 返回移除的元素
+ */
+export function remove(arr, o) {
+    var el = null;
+    var index = arr.indexOf(o);
+    if (index != -1) {
+        el = arr.splice(index, 1);
+        el = el.length ? el[0] : null;
+    }
+    return el;
+};
+
+
+/**
+ * 创建一个拥有特定名称的日志格式话输出函数
+ * @param   {String}    name    日志名称
+ * @returns {Function}
+ */
+export function logger(name) {
+    return function () {
+        let args = Array.prototype.slice.call(arguments);
+        console.log(name + ':', ...args);
+    }
+};
+
+/**
+ * 产生范围数据
+ * @param   {Number}    start   起始数字
+ * @param   {Number}    end     结束数字
+ * @returns {Array} 返回产生的数组
+ */
+export function range(start, end) {
+    let result = [];
+    let reverse = start > end;
+    if (start === end) {
+        result.push(start);
+    } else {
+        if (reverse) {
+            let tmp = start;
+            start = end;
+            end = tmp;
+        }
+        for (let i = start; i < end; i++) {
+            result.push(i);
+        }
+    }
+    if (reverse) {
+        result = result.reverse();
+    }
+    return result;
+};
